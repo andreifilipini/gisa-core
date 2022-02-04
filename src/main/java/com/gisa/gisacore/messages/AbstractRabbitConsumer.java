7c70a8b2bfc0218adf7244ab4678d8a971e25138
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-04 07:51:44 -0300

    abstract rabbit consumer

---
+++ src/main/java/com/gisa/gisacore/messages/AbstractRabbitConsumer.java
@@ -0,0 +1,36 @@
+package com.gisa.gisacore.messages;
+
+import com.gisa.gisacore.exception.InfraException;
+import com.gisa.gisacore.util.StringUtil;
+import org.slf4j.Logger;
+
+public abstract class AbstractRabbitConsumer {
+
+    protected abstract Logger getLogger();
+
+    protected abstract void receive(String body);
+
+    protected abstract void execute(String body);
+
+    protected void executeLoggin(String body) {
+        try {
+            execute(body);
+        } catch (InfraException ie) {
+            logWarn(ie.getMessage(), body);
+        } catch (Exception e) {
+            logError(e.getMessage(), body);
+        }
+    }
+
+    protected void logWarn(String message, String body) {
+        getLogger().warn(formatErrorMessage(message, body));
+    }
+
+    protected void logError(String message, String body) {
+        getLogger().error(formatErrorMessage(message, body));
+    }
+
+    private String formatErrorMessage(String message, String body) {
+        return String.format("message: %s; body: %s", message, StringUtil.isBlank(body) ? "null" : body);
+    }
+}
